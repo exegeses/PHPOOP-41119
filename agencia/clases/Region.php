78ea45c98b76fa03f3798e25441e60b216bd02db
marcos pinardi <exegeses@gmail.com>
2020-08-22 12:33:33 -0300

    admin de regiones

---
+++ agencia/clases/Region.php
@@ -0,0 +1,38 @@
+<?php
+
+    class Region
+    {
+        private $regID;
+        private $regNombre;
+
+        public function listarRegiones()
+        {
+            $link = Conexion::conectar();
+            $sql = "SELECT regID, regNombre
+                        FROM regiones";
+            $stmt = $link->prepare($sql);
+            $stmt->execute();
+
+            $regiones = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $regiones;
+        }
+
+        public function getRegID()
+        {
+            return $this->regID;
+        }
+        public function setRegID($regID): void
+        {
+            $this->regID = $regID;
+        }
+
+        public function getRegNombre()
+        {
+            return $this->regNombre;
+        }
+        public function setRegNombre($regNombre): void
+        {
+            $this->regNombre = $regNombre;
+        }
+
+    }
