9474ac5ff3bc7073a5f6c8aea5489733bec7a9ba
marcos pinardi <exegeses@gmail.com>
2020-09-05 10:35:10 -0300

    método agregarRegion()

--- agencia/clases/Region.php
+++ agencia/clases/Region.php
@@ -7,8 +7,9 @@
 
 
         /**
-         * @return array
-         */
+         * método para listar regiones
+         * @return array $regiones
+         * */
         public function listarRegiones()
         {
             $link = Conexion::conectar();
@@ -40,6 +41,28 @@
 
         }
 
+        public function agregarRegion()
+        {
+            $regNombre = $_POST['regNombre'];
+            $link = Conexion::conectar();
+
+            $sql = "INSERT INTO regiones
+                            ( regNombre ) 
+                        VALUES
+                            ( :regNombre )";
+
+            $stmt = $link->prepare($sql);
+            $stmt->bindParam(':regNombre', $regNombre, PDO::PARAM_STR);
+
+            if( $stmt->execute() ){
+                // registramos atributos en el objeto
+                $this->setRegNombre($regNombre);
+                $this->setRegID( $link->lastInsertId() );
+                return true;
+            }
+            return false;
+        }
+
         public function modificarRegion()
         {
             $regID = $_POST['regID'];
@@ -62,7 +85,9 @@
         }
 
         ### TAREA: hacer agregarRegion()
-        
+
+
+        ######## getters & setters
         public function getRegID()
         {
             return $this->regID;
